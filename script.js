4f6849c8949e8aacc96d9fe9ef619b47c26e563f
Johan <62746961+johanjohanss@users.noreply.github.com>
2021-05-17 14:08:55 +0200

    navigation with functions

--- script.js
+++ script.js
@@ -21,6 +21,28 @@ function showNewOrderPage(){
 
 function showStartPage(){
     console.log("showing startpage");
-    newOrderPage.classList.add("d-none");
-    startPage.classList.remove("d-none");
+    hidePage(newOrderPage);
+    showPage(startPage);
+}
+
+
+function showPage(page){
+    page.classList.remove("d-none");
+}
+
+//Skicka in minst en sida att dölja men kan också skicka in fler
+//Om inga fler sidor skickas in sätts de till null
+function hidePage(page1, page2, page3) {
+    page2 = page2 || null;
+    page3 = page3 || null;
+  
+    //Hide pages
+    page1.classList.add("d-none");
+
+    if(page2 != null){
+        page2.classList.add("d-none");
+    }
+    if(page3 != null){
+        page3.classList.add("d-none");
+    }
 }
