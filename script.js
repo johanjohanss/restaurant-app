61f6a73f3c50b67344bbe9089d45bdab047efecc
Johan <62746961+johanjohanss@users.noreply.github.com>
2021-05-19 13:48:07 +0200

    show order information event listener

--- script.js
+++ script.js
@@ -103,9 +103,19 @@ function updateOrders(){
         sumDiv.appendChild(sum)
 
         let symbolDiv = document.createElement("div");
+        let minusDiv = document.createElement("div");
+        let infoDiv = document.createElement("div");
+        
+        //Event listener så när man klickar på info så kan info för ordern visas i showOrderInformation
+        infoDiv.addEventListener("click", function(){
+            showOrderInformation(order);
+        })
+
         symbolDiv.classList.add("col-4");
-        symbolDiv.innerHTML = '<svg class="symbol minus float-right" xmlns="http://www.w3.org/2000/svg" width="2.3rem" fill="currentColor" class="bi bi-dash-square-fill" viewBox="0 0 16 16"><path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm2.5 7.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1 0-1z"/></svg>'
-        symbolDiv.innerHTML += '<svg class="symbol info float-right mr-2" xmlns="http://www.w3.org/2000/svg" width="2.3rem" fill="currentColor" class="bi bi-info-square-fill" viewBox="0 0 16 16"><path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm8.93 4.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM8 5.5a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/></svg>'
+        minusDiv.innerHTML = '<svg class="symbol minus float-right" xmlns="http://www.w3.org/2000/svg" width="2.3rem" fill="currentColor" class="bi bi-dash-square-fill" viewBox="0 0 16 16"><path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm2.5 7.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1 0-1z"/></svg>'
+        infoDiv.innerHTML += '<svg class="symbol info float-right mr-2" xmlns="http://www.w3.org/2000/svg" width="2.3rem" fill="currentColor" class="bi bi-info-square-fill" viewBox="0 0 16 16"><path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm8.93 4.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM8 5.5a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/></svg>'
+        symbolDiv.appendChild(minusDiv);
+        symbolDiv.appendChild(infoDiv);
 
         row.appendChild(titleDiv);
         row.appendChild(sumDiv);
@@ -134,6 +144,12 @@ function updateOrders(){
         </div>
     </div>
 */
+
+//Funktion som visar kvitto. Ska visa upp alla saker i ordern + summa
+function showOrderInformation(order){
+    console.log("show order information");
+    console.log(order);
+}
 
 //Funktion som bygger upp menyn baserat på menu-objektet längst upp i filen
 function buildMenu(){
