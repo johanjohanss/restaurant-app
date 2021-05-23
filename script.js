837d841df33b34c7c1ab0350cae64c2f5a0a1978
Johan <62746961+johanjohanss@users.noreply.github.com>
2021-05-23 22:28:46 +0200

    continue with order text when basket is not empty

--- script.js
+++ script.js
@@ -572,6 +572,17 @@ function showStartPage(){
     console.log("showing startpage");
     hidePage(newOrderPage, currentOrderPage, orderInformationPage);
     showPage(startPage);
+
+    let startPageNext = document.getElementById("start-page-next");
+    let newOrderButton = document.getElementById("new-order-button");
+    if(basket.length > 0){
+        startPageNext.innerText = "Fortsätt med order";
+        newOrderButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="2rem" fill="currentColor" class="bi bi-arrow-right-square-fill" viewBox="0 0 16 16"><path d="M0 14a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2a2 2 0 0 0-2 2v12zm4.5-6.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5a.5.5 0 0 1 0-1z"/></svg>';
+    }else{
+        startPageNext.innerText = "Ny beställning";
+        newOrderButton.innerHTML = '<svg class="symbol plus float-right" xmlns="http://www.w3.org/2000/svg" width="2.3rem" fill="currentColor" class="bi bi-plus-square-fill" viewBox="0 0 16 16"><path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6.5 4.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3a.5.5 0 0 1 1 0z"/></svg>';
+    }
+
 }
 
 //Visar sidan som skickas in
